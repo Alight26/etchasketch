6f845e01f540e62a9b907174536424c15973b782
Lucas <luc.coulson6@gmail.com>
2025-02-20 17:30:22 -0500

    grid size is now made
    creates the number of divs used in the grid
    using a for loop
    
    Now i need to organize these divs properly
    instead of them just stacking up

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const container = document.querySelector(".container");
-const gridDiv = document.createElement('div');
-container.appendChild(gridDiv);
+const container = document.querySelector(".container"); // imported container Div from HTML
+
+
+let gridSize = 5; // this is going to be user input line under
+// let gridSize = prompt('Enter size of grid: ')
+
+for (let i = 0; i < gridSize ** 2; i++) {
+    const gridDiv = document.createElement('div'); // creates a div using DOM
+    gridDiv.innerText = `Div ${i + 1}`;
+    container.appendChild(gridDiv); // puts new Div inside of container Div as a child
+}
+
