00b1ebea3fb865eed92f93720ca2960d3b697b2f
Lucas <luc.coulson6@gmail.com>
2025-02-20 17:53:12 -0500

    Grid now wraps with the screen

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const container = document.querySelector(".container"); // imported container Di
 let gridSize = 5; // this is going to be user input line under
 // let gridSize = prompt('Enter size of grid: ')
 
+
+// creates the amount of divs that will be used in the grid
 for (let i = 0; i < gridSize ** 2; i++) {
     const gridDiv = document.createElement('div'); // creates a div using DOM
     gridDiv.innerText = `Div ${i + 1}`;
     container.appendChild(gridDiv); // puts new Div inside of container Div as a child
 }
+
+
 
