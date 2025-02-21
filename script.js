d78873c6fe8c1b3922c8c5b99ae884e48b4ebf82
Lucas <luc.coulson6@gmail.com>
2025-02-20 19:40:22 -0500

    etchasketch finished!!
    added mouseover element in the loop which lets
    you draw in the etchasketch!

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const container = document.querySelector(".container"); // imported container Div from HTML
 
 
-let gridSize = 5; // this is going to be user input line under
+let gridSize = 64; // this is going to be user input line under
 // let gridSize = prompt('Enter size of grid: ')
 
 
 // creates the amount of divs that will be used in the grid
 for (let i = 0; i < gridSize ** 2; i++) {
-    const gridDiv = document.createElement('div'); // creates a div using DOM
-    gridDiv.innerText = `Div ${i + 1}`;
+    const gridDiv = document.createElement('div',); // creates a div using DOM
+    gridDiv.classList.add = "grid"; 
+    gridDiv.addEventListener("mouseover", () => {
+        gridDiv.style.backgroundColor = "#D3D3D3";
+    })
     container.appendChild(gridDiv); // puts new Div inside of container Div as a child
 }
-
 
 
